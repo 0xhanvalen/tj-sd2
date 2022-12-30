e023c73edf42066629abc79826415677246723b7
Mack Bowes <mackenziepbowes@gmail.com>
2022-12-30 07:30:02 -0800

    added sd-2.1

--- download.py
+++ download.py
@@ -16,7 +16,7 @@ def download_model():
     )
 
     model = StableDiffusionPipeline.from_pretrained(
-        "CompVis/stable-diffusion-v1-4", 
+        "stabilityai/stable-diffusion-2-1", 
         scheduler=lms,
         use_auth_token=HF_AUTH_TOKEN
     )
